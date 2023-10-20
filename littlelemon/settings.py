41992cd52d926386018fcd4e42782aeeeecc31be
javiermal <javiermal@google.com>
2023-10-20 12:31:08 -0600

    Add tables to database

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+		'USER': 'root',
+		'PASSWORD': 'password',
+		'HOST': '127.0.0.1',
+		'PORT': '3306',
+		'OPTIONS':{
+			'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+		}
     }
 }
 
@@ -122,5 +129,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # This is where you add your project-level static files
+    os.path.join(BASE_DIR, 'static'), # This is where you add your project-level static files
 ]
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    ID = models.IntegerField(primary_key = True) 
+    Title = models.CharField(max_length=254)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField() 
+
+    def __str__(self):
+        return self.Title
+
+
+class Booking(models.Model):
+    ID = models.IntegerField(primary_key = True) 
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.PositiveSmallIntegerField()
+    BookingDate = models.DateTimeField()
+
+    def __str__(self):
+        return self.Name
+
+
+    
